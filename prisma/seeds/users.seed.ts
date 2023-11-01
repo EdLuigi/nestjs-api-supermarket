96ae54385dc27706d892e9c5b88c9947548d2258
Eduardo Ciuffi <luigi.ciuffi@hotmail.com>
2023-11-01 17:34:27 -0300

    feat: add user w/ no userRole in seeds

--- prisma/seeds/users.seed.ts
+++ prisma/seeds/users.seed.ts
@@ -1,23 +1,31 @@
 import { User } from '@/users/entities/user.entity';
 
+// password: 123
 export const usersSeed: User[] = [
   {
-    name: 'user admin',
+    name: '[user admin]',
     registry: 'user1',
     password:
       '$argon2id$v=19$m=65536,t=3,p=4$CTw1zYIJlYncRVNwmbbVog$vsDZBiy5heHyw1r8TuqwBm8eEBnCOzuuVJE9NTUow10',
     createdAt: new Date(),
   },
   {
-    name: 'user manager',
+    name: '[user manager]',
     registry: 'user2',
     password:
       '$argon2id$v=19$m=65536,t=3,p=4$CTw1zYIJlYncRVNwmbbVog$vsDZBiy5heHyw1r8TuqwBm8eEBnCOzuuVJE9NTUow10',
     createdAt: new Date(),
   },
   {
-    name: 'user employee',
+    name: '[user employee]',
     registry: 'user3',
+    password:
+      '$argon2id$v=19$m=65536,t=3,p=4$CTw1zYIJlYncRVNwmbbVog$vsDZBiy5heHyw1r8TuqwBm8eEBnCOzuuVJE9NTUow10',
+    createdAt: new Date(),
+  },
+  {
+    name: '[user w/ no userRole]',
+    registry: 'user4',
     password:
       '$argon2id$v=19$m=65536,t=3,p=4$CTw1zYIJlYncRVNwmbbVog$vsDZBiy5heHyw1r8TuqwBm8eEBnCOzuuVJE9NTUow10',
     createdAt: new Date(),
